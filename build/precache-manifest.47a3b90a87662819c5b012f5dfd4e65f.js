0eebefd625c4ba94abc2ad3ddad638b52af667ad
RJ <melross.allado@gmail.com>
2021-04-12 10:56:21 +0800

    Fix

---
+++ build/precache-manifest.47a3b90a87662819c5b012f5dfd4e65f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44b67f6ff897c802f9df8abe79e34d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2ac0e219325e0df2894",
+    "url": "./static/css/main.2fe4883f.chunk.css"
+  },
+  {
+    "revision": "f29a7ea457004a21393e",
+    "url": "./static/js/2.b0174262.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b0174262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ac0e219325e0df2894",
+    "url": "./static/js/main.d9cf5d07.chunk.js"
+  },
+  {
+    "revision": "aa4f5661cc6856720932",
+    "url": "./static/js/runtime-main.f56665fd.js"
+  }
+]);
