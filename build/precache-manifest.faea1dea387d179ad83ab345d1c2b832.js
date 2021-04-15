7a87be064fac159b93ea922f85815e3dd522200f
RJ <melross.allado@gmail.com>
2021-04-15 14:31:56 +0800

    commit

---
+++ build/precache-manifest.faea1dea387d179ad83ab345d1c2b832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ab7f7a43f577c915d171c372ffd502",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31c9782cd8bed3af0009",
+    "url": "./static/css/main.d3b56157.chunk.css"
+  },
+  {
+    "revision": "f29a7ea457004a21393e",
+    "url": "./static/js/2.b0174262.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b0174262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c9782cd8bed3af0009",
+    "url": "./static/js/main.52af3782.chunk.js"
+  },
+  {
+    "revision": "aa4f5661cc6856720932",
+    "url": "./static/js/runtime-main.f56665fd.js"
+  }
+]);
