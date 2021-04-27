021820bc9c80ae5d66a81dc42f76a8e39b2084c1
RJ <melross.allado@gmail.com>
2021-04-27 14:51:15 +0800

    Push latest update

---
+++ build/precache-manifest.cc31adf3823b2dd36f5ee6dc86cff6b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149d09a233b43edd81f0f1c1a2a51d09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2169a9cb411d833ea497",
+    "url": "./static/css/main.0f45c5b3.chunk.css"
+  },
+  {
+    "revision": "f29a7ea457004a21393e",
+    "url": "./static/js/2.b0174262.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b0174262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2169a9cb411d833ea497",
+    "url": "./static/js/main.9942b5b9.chunk.js"
+  },
+  {
+    "revision": "aa4f5661cc6856720932",
+    "url": "./static/js/runtime-main.f56665fd.js"
+  }
+]);
